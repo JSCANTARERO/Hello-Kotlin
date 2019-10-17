a470ba270c3c6b9a02526b278ca289d936c6c584
JorgeSanchez <js7170206@gmail.com>
2019-10-16 22:51:20 -0600

    Creacion de clase Car y prueba del funcionanmiento

--- Hello Kotlin/src/Main.kt
+++ Hello Kotlin/src/Main.kt
@@ -5,11 +5,79 @@ val credits: String = """
     Date 'n' time: 16/10/19 7:07pm
 """.trimIndent()
 
+var greetings: String? = null
+
 fun main() {
-    println("Hello world")
-    val name: String = "George"
-    var
-    if()
-    println(name)
-    //println(credits)
+    println(credits)
+    //println("Hello world")
+    /*val name: String = "George"
+    var counter: Int = 0
+    do{
+        println(name)
+        println(credits)
+        //counter ++
+
+        when(greetings) {
+            null -> println("\nHi")
+            else -> println(greetings)
+        }
+        Thread.sleep(10000)
+    }while (counter != 1)*/
+
+    //propiedades o caracteristicas
+
+    //var objectName: Class = Initialize
+
+    var ferrari: Car = Car()
+    ferrari.turnOn()
+
+}
+
+class Car{
+    var mark: String = ""
+    var color: String = ""
+    var price: Int = 0
+    var maxSpeed: Int = 0
+    var fuel: Int = 20
+    var distance: Int = 0
+    var speed: Int = 0
+    var time: Int = 0
+
+    fun turnOn(){
+        println("\nThe car has turned on")
+        println("The fuel tank is full")
+        do {
+            println("\n")
+            accelerate()
+            distance = distance + (speed * 5)
+            println("The car's speed is: $speed m/s")
+            println("$time seconds have passed")
+            time = time + 10
+            println("Distance traveled: $distance")
+            fuel = fuel -5
+            println("The fuel tank have: $fuel")
+            speed = speed + 5
+            if(speed >= 30)
+            {
+                println("Max speed reached")
+                speed = 30
+            }
+            Thread.sleep(10000)
+        }while (fuel > 0)
+        if(fuel == 0)
+        {
+            println("The fuel tank is empty")
+            turnOff()
+            stop()
+        }
+    }
+    fun turnOff(){
+        println("The car has turned off")
+    }
+    fun accelerate(){
+        println("The car is accelerating")
+    }
+    fun stop(){
+        println("The car has stopped")
+    }
 }
